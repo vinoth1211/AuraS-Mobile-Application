54af7d7e8112eccd9a46b8a56528ddc76fcf90b9
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-05-28 14:58:17 +0530

    fixed connecting the emulator

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     // Add the Google services Gradle plugin
@@ -10,8 +10,8 @@ plugins {
 
 android {
     namespace = "com.example.skincare_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,10 +27,10 @@ android {
         applicationId = "com.example.skincare_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {

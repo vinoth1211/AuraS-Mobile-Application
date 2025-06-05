5ad8e47f3e716c1064f152729686d362cbcdb2f3
KTGamage <kasuntharaka18628@gmail.com>
2025-06-05 13:46:48 +0530

    create appointment model

---
+++ lib/appointment_model.dart
@@ -0,0 +1,38 @@
+// appointment_model.dart
+class Appointment {
+  final String id;
+  final String userId;
+  final String dermatologistId;
+  final DateTime date;
+  final TimeOfDay time;
+  final String status; // 'pending', 'confirmed', 'canceled'
+  final String userName;
+  final String userEmail;
+  final String userPhone;
+
+  Appointment({
+    required this.id,
+    required this.userId,
+    required this.dermatologistId,
+    required this.date,
+    required this.time,
+    required this.status,
+    required this.userName,
+    required this.userEmail,
+    required this.userPhone,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'dermatologistId': dermatologistId,
+      'date': Timestamp.fromDate(date),
+      'time': '${time.hour}:${time.minute}',
+      'status': status,
+      'userName': userName,
+      'userEmail': userEmail,
+      'userPhone': userPhone,
+      'createdAt': Timestamp.now(),
+    };
+  }
+}
